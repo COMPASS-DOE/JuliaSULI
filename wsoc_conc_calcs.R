362096c8fe1d0ccd1602a036aad27cc3ee03acae
Julia McElhinny <julia.mcelhinny@charter.net>
2022-11-22 12:33:32 -0800

    Create wsoc_conc_calcs.R

---
+++ wsoc_conc_calcs.R
@@ -0,0 +1,11 @@
+## This script reads in fully processed NPOC/TDN data and a csv file with
+## WSOM soil extraction procedure notes to calculate WSOC and WSON 
+## concentrations normalized to the amount of water and soil used in the
+## extraction process.
+##
+## Data are saved in the JuliaSULI repository.
+
+## Created: 2022/11/22
+## Julia McElhinny
+
+## 1. Load Packages and Data ---------------------------------------------------
